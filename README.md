3e272bbba7a018889da42224b67c9f616e89a9bf
alexakarp <45791739+alexakarp@users.noreply.github.com>
2018-12-17 17:32:39 +0100

    fixes according to notes
    notes in notes-on-kuber_maas.md

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# kuber_maas
+# kuber-maas
+
+1. Figure out how to deploy/maintain a Kubernetes cluster in a MAAS infrastructure. We know that the conjure-up tool-chain provides an easy way to achieve that, although it's probably too large for the anticipated use-case. A first iteration could wrap conjure-up  to transparently deploy Kubernetes (those deploys can be automated with the use of Conjurefiles). Focus in writing a pluggable code, in case conjure-up needs to be replaced by another deployment tool. Find a desirable configuration for Kubernetes for automated deployment in MAAS. 
+
+→ first conjure-up needs to be installed on the local machine. If you don't have 'snap' installed on your system yet, make sure to install it:
+
+```sh
+$ sudo apt update
+$ sudo apt install snapd
+```
+Afterwards, conjure-up should be installed with snap:
+
+```sh
+$ sudo snap install conjure-up --classic
+```
+
+Additional information can be found on the website:
+https://docs.conjure-up.io/stable/en/user-manual#installing-conjure-up
+
+We will also need the Kubernetes command-line tool. In order to install it with snap run the following command:
+
+```sh
+$  sudo snap install kubectl --classic
+```
+
+Verify the installation with 
+
+```sh
+$  kubectl version
+```
+
+pull the git repo in a directory:
+
+https://github.com/alexakarp/kuber_maas
+open a terminal in the directory, execute the following commands to run the script
+
+```sh
+$  virtualenv venv
+$  . venv/bin/activate
+$  pip install -r requirements.txt
+$  python app.py
+```
+
+The Console output should be something like this:
+> Serving Flask app "app" (lazy loading)
+> Environment: production
+> WARNING: Do not use the development server in a production environment.
+> Use a production WSGI server instead.
+> Debug mode: on
+> Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+> Restarting with stat
+> No cluster detected. Please deploy a cluster first using the endpoint /maas/
+>  Debugger is active!
+>  Debugger PIN: 119-668-834
+
+
+Now try to deploy the cluster with a POST request
+
+curl -X POST \ 'http://127.0.0.1:5000/maas/?maas_endpoint=localhost&api_key=apikey123%21%21&operation=create'
+
+The app checks, if there is a cluster deployed already. The deployment locally can take 17-20 minutes! After succesful deployment it will output the cluster status and:
+> Installation of your big software is now complete
+
+2. Build a REST service that exposes an endpoint through which is possible to deploy/destroy a Kubernetes cluster in a MAAS pool. The service must know how many clusters it has deployed and how many are currently active, and must be able to access and deploy applications on these clusters. The first iteration could consider that there is either one or zero clusters deployed at any given time.
+
+
+3. Create an endpoint in the aforementioned REST service for deploying applications on top of the Kubernetes cluster. An initial iteration could deploy a simple container with a “hello-world” program.
+
+
+4. Modify the service to include a container building pipeline, making it possible for the user to just provide an application binary and a few specifications. The service would then wrap the binary in a Docker image and build it, then deploying it on top of the Kubernetes cluster.
--- app.py
+++ app.py
@@ -20,13 +20,19 @@ deployment_operator_teardown = 'conjure-down'
 controller = 'localhost-localhost'
 extensions_v1beta1 = client.ExtensionsV1beta1Api()
 DEPLOYMENT_NAME = "nginx-deployment"
+config.load_kube_config()
+services = {}
 
 
 class maas(Resource):
     def get(self):
-        return ('Cluster Running Message'), 404
+        if check_cluster():
+            return ('Cluster already running, no need to conjure-up'), 200
+        return ('Cluster not running'), 404
 
     def post(self):
+        if check_cluster():
+            return 'Cluster already running, no need to conjure-up', 409
         parser = reqparse.RequestParser()
         parser.add_argument('maas_endpoint')
         parser.add_argument('api_key')
@@ -59,79 +65,111 @@ class maas(Resource):
 
 class apps(Resource):
     def get(self):
-        config.load_kube_config()
+        #config.load_kube_config()
+
+        print(services)
         v1 = client.CoreV1Api()
-        print("Listing pods with their IPs: ")
-        bufferstring = ''
+        #print("Listing pods with their IPs: ")
+        #bufferstring = ''
         #just returning pods with their IPs as a check that they are up
-        ret = v1.list_pod_for_all_namespaces(watch=False)
+        #ret = v1.list_pod_for_all_namespaces(watch=False)
 
-        for i in ret.items:
-            bufferstring = bufferstring + i.status.pod_ip + " " + i.metadata.namespace + " " + i.metadata.name + " ### "
+        #for i in ret.items:
+        #    bufferstring = bufferstring + i.status.pod_ip + " " + i.metadata.namespace + " " + i.metadata.name + " ### "
 
-        return bufferstring, 404
+        return str(services), 200
 
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('nr_nodes')
+        parser.add_argument('replicas')
         parser.add_argument('expose')
         parser.add_argument('port')
+        parser.add_argument('name')
+        parser.add_argument('image')
         args = parser.parse_args()
         port_nr = int(args['port'])
-        nodes_nr = int(args['nr_nodes'])
+        replicas = int(args['replicas'])
+        name = args['name']
+        image = args['image']
 
-        api_instance = client.CoreV1Api()
-        service = client.V1Service()
-        #specifying service parameters (can be more parameterized)
-        service.api_version = "v1"
-        service.kind = "Service"
-        service.metadata = client.V1ObjectMeta(name=DEPLOYMENT_NAME)
-        spec = client.V1ServiceSpec()
-        spec.selector = {"app": "MyApp"}
-        spec.type = "NodePort"
-        spec.ports = [client.V1ServicePort(protocol="TCP", port=port_nr)]
-        service.spec = spec
+        if not check_cluster():
+            return 'Please create cluster first with POST to /maas/ endpoint', 500
+
+        if name == '' or name == None:
+            return 'Please specify the app you want to depploy with \'appname\' ', 500;
+
+        if image == '' or image == None:
+            return 'Please specify the image you want to depploy with \'image\' ', 500;
+
 
         extensions_v1beta1 = client.ExtensionsV1beta1Api()
-        deployment = create_deployment_object(nodes_nr)
-        create_deployment(extensions_v1beta1, deployment)
+        deployment = create_deployment_object(name, image, replicas)
+        test = create_deployment(extensions_v1beta1, deployment)
+        api_instance = client.CoreV1Api()
+
 
         #if user wants it exposed, service will be created
         if(args['expose'] == 'true'):
+            service = client.V1Service()
+            # specifying service parameters (can be more parameterized)
+            service.api_version = "v1"
+            service.kind = "Service"
+            service.metadata = client.V1ObjectMeta(name=name)
+            spec = client.V1ServiceSpec()
+            spec.selector = {"app": name}
+            spec.type = "NodePort"
+            spec.ports = [client.V1ServicePort(protocol="TCP", port=port_nr)]
+            service.spec = spec
             api_instance.create_namespaced_service(namespace="default", body=service)
-        return ('created :')
+            service_instance = name + '1234'
+            services[service_instance] = service
+            return ('created: ' + name), 200
+
+
+        return ('created: ' + name), 200
 
     def delete(self):
-        api_instance = client.CoreV1Api()
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('name')
+        parser.add_argument('service')
+        args = parser.parse_args()
+        name = args['name']
+        service = args['service']
         extensions_v1beta1 = client.ExtensionsV1beta1Api()
 
-        #deleting of service not yet working
-        #api_instance.delete_namespaced_service(name=DEPLOYMENT_NAME, namespace="default")
+        if service == 'true':
+            api_instance = client.CoreV1Api()
+            # deleting of service not yet working
+            api_instance.delete_namespaced_service(name='nginx', namespace="default")
+
+
 
         #delete deployment
-        delete_deployment(extensions_v1beta1)
-        return ('deleted')
+        response_msg = delete_deployment(extensions_v1beta1, name)
+        testoutput = str(response_msg.status)
+        return testoutput, 200
 
 
-def create_deployment_object(nr_nodes):
+def create_deployment_object(name, image, replicas):
     # Configureate Pod template container
     container = client.V1Container(
-        name="nginx",
-        image="nginx:1.7.9",
+        name=name,
+        image=image,
         ports=[client.V1ContainerPort(container_port=80)])
     # Create and configurate a spec section
     template = client.V1PodTemplateSpec(
-        metadata=client.V1ObjectMeta(labels={"app": "nginx"}),
+        metadata=client.V1ObjectMeta(labels={"app": name}),
         spec=client.V1PodSpec(containers=[container]))
     # Create the specification of deployment
     spec = client.ExtensionsV1beta1DeploymentSpec(
-        replicas=nr_nodes,
+        replicas=replicas,
         template=template)
     # Instantiate the deployment object
     deployment = client.ExtensionsV1beta1Deployment(
         api_version="extensions/v1beta1",
         kind="Deployment",
-        metadata=client.V1ObjectMeta(name=DEPLOYMENT_NAME),
+        metadata=client.V1ObjectMeta(name=name),
         spec=spec)
 
     return deployment
@@ -144,27 +182,19 @@ def create_deployment(api_instance, deployment):
         namespace="default")
     print("Deployment created. status='%s'" % str(api_response.status))
 
-
-def update_deployment(api_instance, deployment):
-    # Update container image
-    deployment.spec.template.spec.containers[0].image = "nginx:1.9.1"
-    # Update the deployment
-    api_response = api_instance.patch_namespaced_deployment(
-        name=DEPLOYMENT_NAME,
-        namespace="default",
-        body=deployment)
-    print("Deployment updated. status='%s'" % str(api_response.status))
+    return api_response
 
 
-def delete_deployment(api_instance):
+def delete_deployment(api_instance, name):
     # Delete deployment
     api_response = api_instance.delete_namespaced_deployment(
-        name=DEPLOYMENT_NAME,
+        name=name,
         namespace="default",
         body=client.V1DeleteOptions(
             propagation_policy='Foreground',
             grace_period_seconds=5))
     print("Deployment deleted. status='%s'" % str(api_response.status))
+    return api_response
 
 
 def check_cluster():
@@ -173,13 +203,19 @@ def check_cluster():
     my_file = Path(filename)
     if my_file.is_file():
         print('Cluster is already there!')
+        return True
     else:
         print('No cluster detected. Please deploy a cluster first using the endpoint /maas/')
+        return False
 
 
 def main():
     api.add_resource(maas, "/maas/")
     api.add_resource(apps, "/apps/")
+
+
+
+
     check_cluster()
     app.run(debug=True)
 
